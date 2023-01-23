82a6061abf5539934ba75df6e8f51b960a8c3226
Anubhab Ghosh <findghosh@gmail.com>
2023-01-23 14:51:08 +0100

    Add code for the DANSE model and some dataloader utils

---
+++ src/danse.py
@@ -0,0 +1,353 @@
+import numpy as np
+import torch
+from torch.autograd import Variable
+from torch import nn, optim, distributions
+from rnn import RNN_model, push_model, save_model
+from timeit import default_timer as timer
+import sys
+import copy
+import os
+from ..utils.utils import compute_log_prob_normal, create_diag, compute_inverse, count_params, ConvergenceMonitor
+
+class DANSE(nn.Module):
+
+    def __init__(self, n_states, n_obs, mu_w, C_w, H, mu_x0, C_x0, rnn_params_dict):
+        super(DANSE, self).__init__()
+
+        # Initialize the paramters of the state estimator
+        self.n_states = n_states
+        self.n_obs = n_obs
+        
+        # Initializing the parameters of the initial state
+        self.mu_x0 = mu_x0
+        self.C_x0 = C_x0
+
+        # Initializing the parameters of the measurement noise
+        self.mu_w = mu_w
+        self.C_w = C_w
+
+        # Initialize the observation model matrix 
+        self.H = H
+
+        # Initialize the parameters of the RNN
+        self.rnn = RNN_model(**rnn_params_dict)
+
+        # Initialize various means and variances of the estimator
+
+        # Prior parameters
+        self.mu_xt_yt_current = None
+        self.L_xt_yt_current = None
+
+        # Marginal parameters
+        self.mu_yt_current = None
+        self.L_yt_current = None
+
+        # Posterior parameters
+        self.mu_xt_yt_prev = None
+        self.L_xt_yt_prev = None
+    
+    def compute_prior_mean_vars(self, mu_xt_yt_prev, L_xt_yt_prev):
+
+        self.mu_xt_yt_prev = mu_xt_yt_prev
+        self.L_xt_yt_prev = create_diag(L_xt_yt_prev)
+
+    def compute_marginal_mean_vars(self):
+
+        self.mu_t_current = self.H @ self.mu_xt_yt_prev.reshape((-1, 1)) + self.mu_w
+        self.L_yt_current = self.H @ self.L_xt_yt_prev @ self.H.T + self.C_w
+    
+    def compute_posterior_mean_vars(self, yt):
+
+        Re_t = compute_inverse(self.H @ self.L_xt_yt_prev @ self.H.T + self.C_w)
+        self.K_t = (self.L_xt_yt_prev @ (self.H.T @ Re_t))
+        self.mu_xt_yt_current = self.mu_xt_yt_prev + self.K_t @ (yt - self.H @ self.mu_xt_yt_prev)
+        self.L_xt_yt_current = self.L_xt_yt_prev - ((self.L_xt_yt_prev @ self.H.T) @ Re_t) @ (self.H @ self.L_xt_yt_prev.T)
+
+    def compute_logprob_batch(self, Yi_batch):
+
+        N, T, _ = Yi_batch.shape
+        log_py_t_given_prev = 0.0 
+        for t in range(T):
+
+            if t >= 1:
+                mu_yt_prev, L_yt_prev_diag = self.rnn(Yi_batch[:, 0:t-1, :])
+            else:
+                mu_yt_prev, L_yt_prev_diag = self.rnn(torch.zeros(N, 1, 1))
+            
+            self.compute_prior_mean_vars(mu_yt_prev, L_yt_prev_diag)
+            self.compute_marginal_mean_vars()
+
+            log_py_t_given_prev += compute_log_prob_normal(
+                X = Yi_batch[:, t, :],
+                mean=self.mu_yt_current,
+                cov=self.L_yt_current
+                ).mean(0)
+
+        return log_py_t_given_prev
+
+def train_danse(model, options, train_loader, val_loader, nepochs, logfile_path, modelfile_path, save_chkpoints, device='cpu', tr_verbose=False):
+
+    model = DANSE(**options)
+    model = push_model(nets=model, device=device)
+    total_num_params, total_num_trainable_params = count_params(model)
+    
+    model.train()
+    optimizer = optim.Adam(model.parameters(), lr=model.lr)
+    #scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.998)
+    #scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=nepochs//3, gamma=0.9) # gamma was initially 0.9
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=nepochs//6, gamma=0.9) # gamma is now set to 0.8
+    criterion = nn.MSELoss() # By default reduction is 'mean'
+    tr_losses = []
+    val_losses = []
+
+    if modelfile_path is None:
+        model_filepath = "./models/"
+    else:
+        model_filepath = modelfile_path
+
+    #if save_chkpoints == True:
+    if save_chkpoints == "all" or save_chkpoints == "some":
+        # No grid search
+        if logfile_path is None:
+            training_logfile = "./log/training_{}_usenorm_{}_NS25000_modified.log".format(model.model_type)
+        else:
+            training_logfile = logfile_path
+    elif save_chkpoints == None:
+        # Grid search
+        if logfile_path is None:
+            training_logfile = "./log/gs_training_{}_usenorm_{}_NS25000_modified.log".format(model.model_type)
+        else:
+            training_logfile = logfile_path
+    
+    # Call back parameters
+    
+    patience = 0
+    num_patience = 3 
+    min_delta = options["min_delta"] # 1e-3 for simpler model, for complicated model we use 1e-2
+    #min_tol = 1e-3 # for tougher model, we use 1e-2, easier models we use 1e-5
+    check_patience=False
+    best_val_loss = np.inf
+    tr_loss_for_best_val_loss = np.inf
+    best_model_wts = None
+    best_val_epoch = None
+    orig_stdout = sys.stdout
+    f_tmp = open(training_logfile, 'a')
+    sys.stdout = f_tmp
+    
+
+    # Convergence monitoring (checks the convergence but not ES of the val_loss)
+    model_monitor = ConvergenceMonitor(tol=min_delta,
+                                    max_epochs=num_patience)
+
+    # This checkes the ES of the val loss, if the loss deteriorates for specified no. of
+    # max_epochs, stop the training
+    #model_monitor = ConvergenceMonitor_ES(tol=min_tol, max_epochs=num_patience)
+
+    print("------------------------------ Training begins --------------------------------- \n")
+    print("Config: {} \n".format(options))
+    print("\n Config: {} \n".format(options), file=orig_stdout)
+    print("No. of trainable parameters: {}\n".format(total_num_trainable_params), file=orig_stdout)
+    print("No. of trainable parameters: {}\n".format(total_num_trainable_params))
+
+    # Start time
+    starttime = timer()
+    try:
+        for epoch in range(nepochs):
+            
+            tr_running_loss = 0.0
+            tr_loss_epoch_sum = 0.0
+            val_loss_epoch_sum = 0.0
+        
+            for i, data in enumerate(train_loader, 0):
+            
+                tr_inputs_batch, tr_targets_batch = data
+                optimizer.zero_grad()
+                X_train = Variable(tr_inputs_batch, requires_grad=False).type(torch.FloatTensor).to(device)
+                tr_predictions_batch = model.forward(X_train)
+                tr_targets_batch = torch.cat((tr_targets_batch[:, :-2, :], torch.sqrt(tr_targets_batch[:, -2:, :])), dim=1) # Ensure that the network models std.devs
+                tr_loss_batch = criterion(tr_predictions_batch, tr_targets_batch.squeeze(2).to(device))
+                tr_loss_batch.backward()
+                optimizer.step()
+                #scheduler.step()
+
+                # print statistics
+                tr_running_loss += tr_loss_batch.item()
+                tr_loss_epoch_sum += tr_loss_batch.item()
+
+                if i % 100 == 99 and ((epoch + 1) % 100 == 0):    # print every 10 mini-batches
+                    #print("Epoch: {}/{}, Batch index: {}, Training loss: {}".format(epoch+1, nepochs, i+1, tr_running_loss / 100))
+                    #print("Epoch: {}/{}, Batch index: {}, Training loss: {}".format(epoch+1, nepochs, i+1, tr_running_loss / 100), file=orig_stdout)
+                    tr_running_loss = 0.0
+            
+            scheduler.step()
+
+            endtime = timer()
+            # Measure wallclock time
+            time_elapsed = endtime - starttime
+
+            with torch.no_grad():
+                
+                for i, data in enumerate(val_loader, 0):
+                    
+                    val_inputs_batch, val_targets_batch = data
+                    X_val = Variable(val_inputs_batch, requires_grad=False).type(torch.FloatTensor).to(device)
+                    val_predictions_batch = model.forward(X_val)
+                    val_targets_batch = torch.cat((val_targets_batch[:, :-2, :], torch.sqrt(val_targets_batch[:, -2:, :])), dim=1) # Ensure that the network models std.devs
+                    val_loss_batch = criterion(val_predictions_batch, val_targets_batch.squeeze(2).to(device))
+                    # print statistics
+                    val_loss_epoch_sum += val_loss_batch.item()
+            
+            # Loss at the end of each epoch
+            tr_loss = tr_loss_epoch_sum / len(train_loader)
+            val_loss = val_loss_epoch_sum / len(val_loader)
+
+            # Record the validation loss per epoch
+            if (epoch + 1) > 100: # nepochs/6 for complicated, 100 for simpler model
+                model_monitor.record(val_loss)
+
+            # Displaying loss at an interval of 200 epochs
+            if tr_verbose == True and (((epoch + 1) % 100) == 0 or epoch == 0):
+                
+                print("Epoch: {}/{}, Training MSE Loss:{:.9f}, Val. MSE Loss:{:.9f} ".format(epoch+1, 
+                model.num_epochs, tr_loss, val_loss), file=orig_stdout)
+                #save_model(model, model_filepath + "/" + "{}_ckpt_epoch_{}.pt".format(model.model_type, epoch+1))
+
+                print("Epoch: {}/{}, Training MSE Loss:{:.9f}, Val. MSE Loss:{:.9f}, Time_Elapsed:{:.4f} secs".format(epoch+1, 
+                model.num_epochs, tr_loss, val_loss, time_elapsed))
+            
+            # Checkpointing the model every few  epochs
+            #if (((epoch + 1) % 500) == 0 or epoch == 0) and save_chkpoints == True:     
+            if (((epoch + 1) % 100) == 0 or epoch == 0) and save_chkpoints == "all": 
+                # Checkpointing model every few epochs, in case of grid_search is being done, save_chkpoints = None
+                save_model(model, model_filepath + "/" + "{}_usenorm_{}_ckpt_epoch_{}.pt".format(model.model_type, epoch+1))
+            elif (((epoch + 1) % nepochs) == 0) and save_chkpoints == "some": 
+                # Checkpointing model at the end of training epochs, in case of grid_search is being done, save_chkpoints = None
+                save_model(model, model_filepath + "/" + "{}_usenorm_{}_ckpt_epoch_{}.pt".format(model.model_type, epoch+1))
+            
+            # Save best model in case validation loss improves
+            '''
+            best_val_loss, best_model_wts, best_val_epoch, patience, check_patience = callback_val_loss(model=model,
+                                                                                                    best_model_wts=best_model_wts,
+                                                                                                    val_loss=val_loss,
+                                                                                                    best_val_loss=best_val_loss,
+                                                                                                    best_val_epoch=best_val_epoch,
+                                                                                                    current_epoch=epoch+1,
+                                                                                                    patience=patience,
+                                                                                                    num_patience=num_patience,
+                                                                                                    min_delta=min_delta,
+                                                                                                    check_patience=check_patience,
+                                                                                                    orig_stdout=orig_stdout)
+            if check_patience == True:
+                print("Monitoring validation loss for criterion", file=orig_stdout)
+                print("Monitoring validation loss for criterion")
+            else:
+                pass
+            
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss # Save best validation loss
+                tr_loss_for_best_val_loss = tr_loss # Training loss corresponding to best validation loss
+                best_val_epoch = epoch+1 # Corresponding value of epoch
+                best_model_wts = copy.deepcopy(model.state_dict()) # Weights for the best model
+            '''
+            # Saving every value
+            tr_losses.append(tr_loss)
+            val_losses.append(val_loss)
+
+            # Check monitor flag
+            if model_monitor.monitor(epoch=epoch+1) == True:
+
+                if tr_verbose == True:
+                    print("Training convergence attained! Saving model at Epoch: {}".format(epoch+1), file=orig_stdout)
+                
+                print("Training convergence attained at Epoch: {}!".format(epoch+1))
+                # Save the best model as per validation loss at the end
+                best_val_loss = val_loss # Save best validation loss
+                tr_loss_for_best_val_loss = tr_loss # Training loss corresponding to best validation loss
+                best_val_epoch = epoch+1 # Corresponding value of epoch
+                best_model_wts = copy.deepcopy(model.state_dict()) # Weights for the best model
+                #print("\nSaving the best model at epoch={}, with training loss={}, validation loss={}".format(best_val_epoch, tr_loss_for_best_val_loss, best_val_loss))
+                #save_model(model, model_filepath + "/" + "{}_usenorm_{}_ckpt_epoch_{}.pt".format(model.model_type, usenorm_flag, epoch+1))
+                break
+
+            #else:
+
+                #print("Model improvement attained at Epoch: {}".format(epoch+1))
+                #best_val_loss = val_loss # Save best validation loss
+                #tr_loss_for_best_val_loss = tr_loss # Training loss corresponding to best validation loss
+                #best_val_epoch = epoch+1 # Corresponding value of epoch
+                #best_model_wts = copy.deepcopy(model.state_dict()) # Weights for the best model
+
+        
+        # Save the best model as per validation loss at the end
+        print("\nSaving the best model at epoch={}, with training loss={}, validation loss={}".format(best_val_epoch, tr_loss_for_best_val_loss, best_val_loss))
+        
+        #if save_chkpoints == True:
+        if save_chkpoints == "all" or save_chkpoints == "some":
+            # Save the best model using the designated filename
+            if not best_model_wts is None:
+                model_filename = "{}_usenorm_{}_ckpt_epoch_{}_best.pt".format(model.model_type, best_val_epoch)
+                torch.save(best_model_wts, model_filepath + "/" + model_filename)
+            else:
+                model_filename = "{}_usenorm_{}_ckpt_epoch_{}_best.pt".format(model.model_type, epoch+1)
+                print("Saving last model as best...")
+                save_model(model, model_filepath + "/" + model_filename)
+        #elif save_chkpoints == False:
+        elif save_chkpoints == None:
+            pass
+    
+    except KeyboardInterrupt:
+
+        if tr_verbose == True:
+            print("Interrupted!! ...saving the model at epoch:{}".format(epoch+1), file=orig_stdout)
+            print("Interrupted!! ...saving the model at epoch:{}".format(epoch+1))
+        else:
+            print("Interrupted!! ...saving the model at epoch:{}".format(epoch+1))
+
+        model_filename = "{}_usenorm_{}_ckpt_epoch_{}_latest.pt".format(model.model_type, epoch+1)
+        torch.save(model, model_filepath + "/" + model_filename)
+
+    print("------------------------------ Training ends --------------------------------- \n")
+    # Restoring the original std out pointer
+    sys.stdout = orig_stdout
+
+    return tr_losses, val_losses, best_val_loss, tr_loss_for_best_val_loss, model
+
+def evaluate_danse(options, test_loader, device, model_file=None, usenorm_flag=0, test_logfile_path = None):
+
+    te_running_loss = 0.0
+    test_loss_epoch_sum = 0.0
+    print("################ Evaluation Begins ################ \n")    
+    
+    # Set model in evaluation mode
+    model = DANSE(**options)
+    model.load_state_dict(torch.load(model_file))
+    criterion = nn.MSELoss()
+    model = push_model(nets=model, device=device)
+    model.eval()
+    if not test_logfile_path is None:
+        test_log = "./log/test_{}_usenorm_{}_trial1.log".format(usenorm_flag, options["model_type"])
+    else:
+        test_log = test_logfile_path
+
+    with torch.no_grad():
+        
+        for i, data in enumerate(test_loader, 0):
+                
+            te_inputs_batch, te_targets_batch = data
+            X_test = Variable(te_inputs_batch, requires_grad=False).type(torch.FloatTensor).to(device)
+            test_predictions_batch = model.forward(X_test)
+            te_targets_batch = torch.cat((te_targets_batch[:, :-2, :], torch.sqrt(te_targets_batch[:, -2:, :])), dim=1) # Ensure that the network models std.devs
+            test_loss_batch = criterion(test_predictions_batch, te_targets_batch.squeeze(2).to(device))
+            # print statistics
+            test_loss_epoch_sum += test_loss_batch.item()
+
+    test_loss = test_loss_epoch_sum / len(test_loader)
+
+    print('Test loss: {:.3f} using weights from file: {} %'.format(test_loss, model_file))
+
+    with open(test_log, "a") as logfile_test:
+        logfile_test.write('Test loss: {:.3f} using weights from file: {}'.format(test_loss, model_file))    
+        
+    
+
+
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,10 @@
 import numpy as np
-import scipy
 import torch
+import os
 from torch.distributions import MultivariateNormal
+from torch.utils.data import Dataset, DataLoader
+from collections import deque
+
 
 def count_params(model):
     """
@@ -25,7 +28,7 @@ def sample_from_pdf(distr, N_samples=100):
     samples = distr.sample((N_samples,))    
     return samples
 
-def log_prob_normal(X, mean, cov):
+def compute_log_prob_normal(X, mean, cov):
 
     Lambda_cov, U_cov = torch.linalg.eig(cov)
     logprob_normal_fn = lambda X : torch.real(- 0.5 * X.shape[1] * torch.log(torch.Tensor([2*torch.pi])) - \
@@ -42,8 +45,232 @@ def check_psd_cov(C):
     C = C.detach().cpu().numpy()
     return np.all(np.linalg.eigvals(C) >= 0)
 
+def compute_inverse(X):
+
+    U, S, Vh = torch.svd(X)
+    return Vh @ (torch.diag(1/S.reshape((-1,))) @ U.T) 
+
 def create_diag(x):
     return torch.diag(x)
+
+class Series_Dataset(Dataset):
+
+    def __init__(self, Z_pM_dict):
+
+        self.data_dict = Z_pM_dict
+        self.num_realizations = Z_pM_dict["num_realizations"]
+        self.num_trajectories = Z_pM_dict["num_trajectories"]
+        self.trajectory_lengths = Z_pM_dict["trajectory_lengths"]
+
+    def __len__(self):
+
+        return len(self.data_dict["data"])
+
+    def __getitem__(self, idx):
+
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        inputs = self.data_dict["data"][idx][1]
+
+        sample = {"inputs": np.expand_dims(inputs, axis=0), 
+                  "targets": self.data_dict["data"][idx][0]
+                  }
+
+        return sample
+
+def obtain_tr_val_test_idx(dataset, tr_to_test_split=0.9, tr_to_val_split=0.83):
+
+    num_training_plus_test_samples = len(dataset)
+    print("Total number of samples: {}".format(num_training_plus_test_samples))
+    print("Training + val to test split: {}".format(tr_to_test_split))
+    print("Training to val split: {}".format(tr_to_val_split))
+
+    num_train_plus_val_samples = int(tr_to_test_split * num_training_plus_test_samples)
+    num_test_samples = num_training_plus_test_samples - num_train_plus_val_samples
+    num_train_samples = int(tr_to_val_split * num_train_plus_val_samples)
+    num_val_samples = num_train_plus_val_samples - num_train_samples
+    
+    indices = torch.randperm(num_training_plus_test_samples).tolist()
+    tr_indices = indices[:num_train_samples]
+    val_indices = indices[num_train_samples:num_train_samples+num_val_samples]
+    test_indices = indices[num_train_samples+num_val_samples:]
+
+    return tr_indices, val_indices, test_indices
+
+def my_collate_fn(batch):
+    inputs = [item["inputs"] for item in batch]
+    targets = [item["targets"] for item in batch]
+    targets = torch.FloatTensor(targets)
+    inputs = torch.from_numpy(np.row_stack(inputs))
+    return (inputs, targets)
+
+def get_dataloaders(dataset, batch_size, tr_indices, val_indices, test_indices=None):
+
+    train_loader = DataLoader(dataset,
+                            batch_size=batch_size,
+                            sampler=torch.utils.data.SubsetRandomSampler(tr_indices),
+                            num_workers=0,
+                            collate_fn=my_collate_fn)
+
+    val_loader = DataLoader(dataset,
+                            batch_size=batch_size,
+                            sampler=torch.utils.data.SubsetRandomSampler(val_indices),
+                            num_workers=0,
+                            collate_fn=my_collate_fn)
+    
+    test_loader = DataLoader(dataset,
+                            batch_size=batch_size,
+                            sampler=torch.utils.data.SubsetRandomSampler(test_indices),
+                            num_workers=0,
+                            collate_fn=my_collate_fn)
+
+    return train_loader, val_loader, test_loader
+
+def create_splits_file_name(dataset_filename, splits_filename):
+    
+    idx_dset_info = dataset_filename.rfind("M")
+    idx_splitfilename = splits_filename.rfind(".pkl")
+    splits_filename_modified = splits_filename[:idx_splitfilename] + "_" + dataset_filename[idx_dset_info:] 
+    return splits_filename_modified
+
+def create_file_paths(params_combination_list, filepath, main_exp_name):
+    
+    list_of_logfile_paths = []
+    # Creating the logfiles
+    for params in params_combination_list:
+
+        exp_folder_name = "trajectories_M{}_P{}_N{}/".format(params["num_trajectories"],
+                                                            params["num_realizations"],
+                                                            params["N_seq"])
+
+        #print(os.path.join(log_filepath, main_exp_name, exp_folder_name))
+        full_path_exp_folder = os.path.join(filepath, main_exp_name, exp_folder_name)
+        list_of_logfile_paths.append(full_path_exp_folder)
+        os.makedirs(full_path_exp_folder, exist_ok=True)
+
+    return list_of_logfile_paths
+
+def get_list_of_config_files(model_type, options, dataset_mode='pfixed', params_combination_list=None, main_exp_name=None):
+    
+    #logfile_path = "./log/estimate_theta_{}/".format(dataset_mode)
+    #modelfile_path = "./models/"
+    if main_exp_name is None:
+        main_exp_name = "{}_L{}_H{}_multiple".format(model_type, 
+                                                     options[model_type]["n_layers"], 
+                                                     options[model_type]["n_hidden"])
+    else:
+        pass
+
+    base_config_dirname = os.path.dirname("./config/configurations_alltheta_pfixed.json")
+    
+    list_of_config_folder_paths = create_file_paths(params_combination_list=params_combination_list,
+                                            filepath=base_config_dirname,
+                                            main_exp_name=main_exp_name)
+
+    #list_of_gs_jsonfile_paths = create_file_paths(params_combination_list=params_combination_list,
+    #                                        filepath=modelfile_path,
+    #                                        main_exp_name=main_exp_name)
+
+    list_of_config_files = []
+    
+    for i, config_folder_path in enumerate(list_of_config_folder_paths):
+        
+        config_filename = "configurations_alltheta_pfixed_gru_M{}_P{}_N{}.json".format(
+            params_combination_list[i]["num_trajectories"], params_combination_list[i]["num_realizations"], 
+            params_combination_list[i]["N_seq"])
+        os.makedirs(config_folder_path, exist_ok=True)
+        config_file_name_full = os.path.join(config_folder_path, config_filename)
+        list_of_config_files.append(config_file_name_full)
+    
+    # Print out the model files
+    #print("Config files to be created at:")
+    #print(list_of_config_files)
+    
+    return list_of_config_files
+
+def check_if_dir_or_file_exists(file_path, file_name=None):
+    flag_dir = os.path.exists(file_path)
+    if not file_name is None:
+        flag_file = os.path.isfile(os.path.join(file_path, file_name))
+    else:
+        flag_file = None
+    return flag_dir, flag_file
+
+class ConvergenceMonitor(object):
+
+    def __init__(self, tol=1e-2, max_epochs=3):
+
+        self.tol = tol
+        self.max_epochs = max_epochs
+        self.convergence_flag = False
+        self.epoch_arr = [] # Empty list to store iteration numbers to check for consecutive iterations
+        self.epoch_count = 0 # Counts the number of consecutive iterations
+        self.epoch_prev = 0 # Stores the value of the previous iteration index
+        self.history = deque()
+
+    def record(self, current_loss):
+
+        if np.isnan(current_loss) == False:
+            
+            # In case current_loss is not a NaN, it will continue to monitor
+            if len(self.history) < 2:
+                self.history.append(current_loss)
+            elif len(self.history) == 2:
+                _ = self.history.popleft()
+                self.history.append(current_loss)
+        
+        else:
+            
+            # Empty the queue in case a NaN loss is encountered during training
+            for _ in range(len(self.history)):
+                _ = self.history.pop()
+    
+    def check_convergence(self):
+
+        if (abs(self.history[0]) > 0) and (abs((self.history[0] - self.history[-1]) / self.history[0]) < self.tol):
+            convergence_flag = True
+        else:
+            convergence_flag = False
+        return convergence_flag
+
+    def monitor(self, epoch):
+
+        if len(self.history) == 2 and self.convergence_flag == False:
+            
+            convg_flag = self.check_convergence()
+
+            #if convg_flag == True and self.epoch_prev == 0: # If convergence is satisfied in first condition itself
+                #print("Iteration:{}".format(epoch))
+            #    self.epoch_count += 1
+            #    self.epoch_arr.append(epoch)
+            #    if self.epoch_count == self.max_epochs:
+            #        print("Exit and Convergence reached after {} iterations for relative change in loss below :{}".format(self.epoch_count, self.tol))   
+            #        self.convergence_flag = True
+
+            #elif convg_flag == True and self.epoch_prev == epoch-1: # If convergence is satisfied
+                #print("Iteration:{}".format(epoch))                                                                        
+            if convg_flag == True and self.epoch_prev == epoch-1: # If convergence is satisfied
+                self.epoch_count += 1 
+                self.epoch_arr.append(epoch)
+                if self.epoch_count == self.max_epochs:
+                    print("Consecutive iterations are:{}".format(self.epoch_arr))
+                    print("Exit and Convergence reached after {} iterations for relative change in NLL below :{}".format(self.epoch_count, self.tol))  
+                    self.convergence_flag = True 
+                
+            else:
+                #print("Consecutive criteria failed, Buffer Reset !!")
+                #print("Buffer State:{} reset!!".format(self.epoch_arr)) # Display the buffer state till that time
+                self.epoch_count = 0
+                self.epoch_arr = []
+                self.convergence_flag = False
+
+            self.epoch_prev = epoch # Set iter_prev as the previous iteration index
+        
+        else:
+            pass
+        
+        return self.convergence_flag
 
 
 
