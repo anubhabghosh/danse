Fix variance again

ssm_models.py
@@ -134,8 +134,8 @@ class LorenzAttractorModel(object):
 
     def init_noise_covs(self):
 
-        self.Q = self.q**2 * np.eye(self.n_states)
-        self.R = self.r**2 * np.eye(self.n_obs)
+        self.Q = self.q * np.eye(self.n_states)
+        self.R = self.r * np.eye(self.n_obs)
         return None
 
     def generate_single_sequence(self, T, inverse_r2_dB, nu_dB):
@@ -190,8 +190,8 @@ class SinusoidalSSM(object):
 
     def init_noise_covs(self):
 
-        self.Q = self.q**2 * np.eye(self.n_states)
-        self.R = self.r**2 * np.eye(self.n_obs)
+        self.Q = self.q * np.eye(self.n_states)
+        self.R = self.r * np.eye(self.n_obs)
         return None
 
     def h_fn(self, x):